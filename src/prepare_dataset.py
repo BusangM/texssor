88da8c1f989a2f24181a4ec9cddb5e86051fdf69
B.U <busang@wethinkcode.co.za>
2024-08-01 23:04:59 +0200

    chore: Preprocess and prepare dataset for analysis

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -1,3 +1,19 @@
 import pandas as pd
+import re
 
-from
+
+def preprocess_text(text):
+    #Note to self: Some models might require to remove punctuation and special characters. Didn't include punctuation removal here for simplicity.
+    return ' '.join(text.lower().split()) # Cleaning up data, removing whitespaces, converting to lowercase 
+
+
+def prepare_dataset(input_file, output_file):
+
+    data = pd.read_csv(input_file)
+    data['preprocessed_text'] = data['response'].apply(preprocess_text)
+    data.to_csv(output_file, index=False)
+
+if __name__ == "__main__":
+    input_file = 'data/responses.csv'
+    output_file = 'data/preprocessed_responses.csv'
+    prepare_dataset(input_file, output_file)
